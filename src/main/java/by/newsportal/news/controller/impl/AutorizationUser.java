b16cf59bdb633ddd7b78beb2b7b5b9c9ba6b5884
EvgeniErmakov <evgeni_ermakov@mail.ru>
2021-08-29 00:19:17 +0300

    fix authorization in userDao

--- src/main/java/by/newsportal/news/controller/impl/AutorizationUser.java
+++ src/main/java/by/newsportal/news/controller/impl/AutorizationUser.java
@@ -1,6 +1,7 @@
 package by.newsportal.news.controller.impl;
 
 import java.io.IOException;
+
 import by.newsportal.news.bean.RegistrationInfo;
 import by.newsportal.news.bean.User;
 import by.newsportal.news.controller.Command;
@@ -11,55 +12,53 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
 public class AutorizationUser implements Command {
-	private static AutorizationUser instance = new AutorizationUser();
-	private static final ServiseProvider PROVIDER = ServiseProvider.getInstance();
-	private static final UserServise USER_SERVISE = PROVIDER.getUserServise();
-	public static final String SESSION_PATH = "path";
-	public static final String PART_PATH = "Controller?command=";
-	public static final String PATH_COMMAND_AUTHORIZATION = "AUTHORIZATION_PAGE";
-	public static final String PATH_COMMAND_ERROR = "UNKNOWN_COMMAND";
-	public static final String PATH_COMMAND_AFTER_AUTHORIZATION = "AFTER_AUTHORIZATION";
-	
+    private static AutorizationUser instance = new AutorizationUser();
+    private static final ServiseProvider PROVIDER = ServiseProvider.getInstance();
+    private static final UserServise USER_SERVISE = PROVIDER.getUserServise();
+    public static final String SESSION_PATH = "path";
+    public static final String PART_PATH = "Controller?command=";
+    public static final String PATH_COMMAND_AUTHORIZATION = "AUTHORIZATION_PAGE";
+    public static final String PATH_COMMAND_ERROR = "UNKNOWN_COMMAND";
+    public static final String PATH_COMMAND_AFTER_AUTHORIZATION = "AFTER_AUTHORIZATION";
 
-	private AutorizationUser() {
-	}
 
-	public static AutorizationUser getInstance() {
-		return instance;
-	}
+    private AutorizationUser() {
+    }
 
-	@Override
-	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String path = PART_PATH + PATH_COMMAND_AUTHORIZATION;
-		RegistrationInfo info = new RegistrationInfo(request);
-		request.getSession(true).setAttribute(SESSION_PATH, PATH_COMMAND_AUTHORIZATION);	
-	
-		try {
-			if(info.getEnteredPassword().equals("")) {
-				path = PART_PATH + PATH_COMMAND_AUTHORIZATION + "&incorrect_data_message=Incorrect data:";
-				response.sendRedirect(path);
-				return;
-			}
+    public static AutorizationUser getInstance() {
+        return instance;
+    }
 
-	User user = USER_SERVISE.authorization(info);
-	
-			request.getSession(true).setAttribute(SESSION_PATH, PATH_COMMAND_AFTER_AUTHORIZATION);
-	request.getSession(true).setAttribute("user", user);
-			path = PART_PATH + PATH_COMMAND_AFTER_AUTHORIZATION;
-		if (user == null) {
-		path =  PART_PATH + PATH_COMMAND_AUTHORIZATION + "&user_not_found=User is not found";
-			} else {
-				
-			request.getSession(true).setAttribute(SESSION_PATH, PATH_COMMAND_AFTER_AUTHORIZATION);
-			request.getSession(true).setAttribute("user", user);
-				path = PART_PATH + PATH_COMMAND_AFTER_AUTHORIZATION;
-			}
-		} catch (ServiseException e) {
-			request.getSession(true).setAttribute(SESSION_PATH, PATH_COMMAND_ERROR);
-			path = PART_PATH + PATH_COMMAND_ERROR;
-		} 
-		response.sendRedirect(path);
-	}
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String path = PATH_COMMAND_ERROR;
+        RegistrationInfo info = new RegistrationInfo(request);
+        request.getSession(true).setAttribute(SESSION_PATH, PATH_COMMAND_AUTHORIZATION);
+
+        try {
+            if (info.getEnteredPassword().equals("") || info.getEmail().equals("")) {
+                path = PART_PATH + PATH_COMMAND_AUTHORIZATION + "&incorrect_data_message=Incorrect data:";
+                response.sendRedirect(path);
+                return;
+            }
+
+            User user = USER_SERVISE.authorization(info);
+
+            request.getSession(true).setAttribute(SESSION_PATH, PATH_COMMAND_AFTER_AUTHORIZATION);
+            request.getSession(true).setAttribute("user", user);
+
+            if (user == null) {
+                path = PATH_COMMAND_AUTHORIZATION + "&incorrect_data_message=User is not found";
+            } else {
+                request.getSession(true).setAttribute(SESSION_PATH, PATH_COMMAND_AFTER_AUTHORIZATION);
+                request.getSession(true).setAttribute("user", user);
+                path = PATH_COMMAND_AFTER_AUTHORIZATION;
+            }
+
+        } catch (ServiseException e) {
+            request.getSession(true).setAttribute(SESSION_PATH, PATH_COMMAND_ERROR);
+        }
+        response.sendRedirect("Controller?command=" + path);
+    }
 }
--- src/main/java/by/newsportal/news/controller/impl/RegistrationUser.java
+++ src/main/java/by/newsportal/news/controller/impl/RegistrationUser.java
@@ -1,6 +1,7 @@
 package by.newsportal.news.controller.impl;
 
 import java.io.IOException;
+
 import by.newsportal.news.bean.RegistrationInfo;
 import by.newsportal.news.bean.User;
 import by.newsportal.news.controller.Command;
@@ -12,47 +13,46 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class RegistrationUser implements Command {
-	private static final RegistrationUser instance = new RegistrationUser();
-	private static final ServiseProvider PROVIDER = ServiseProvider.getInstance();
-	private static final UserServise USER_SERVISE = PROVIDER.getUserServise();
-	private static final String  EMPTY_PASSWORD = "";
-	public static final String SESSION_PATH = "path";
-	public static final String PART_PATH = "Controller?command=";
-	public static final String PATH_COMMAND_REGISTRATION = "REGISTRATION_PAGE";
-	public static final String PATH_COMMAND_ERROR = "UNKNOWN_COMMAND";
-	public static final String PATH_COMMAND_AUTHORIZATION = "AUTHORIZATION_PAGE";
+    private static final RegistrationUser instance = new RegistrationUser();
+    private static final ServiseProvider PROVIDER = ServiseProvider.getInstance();
+    private static final UserServise USER_SERVISE = PROVIDER.getUserServise();
+    private static final String EMPTY_PASSWORD = "";
+    public static final String SESSION_PATH = "path";
+    public static final String PART_PATH = "Controller?command=";
+    public static final String PATH_COMMAND_REGISTRATION = "REGISTRATION_PAGE";
+    public static final String PATH_COMMAND_ERROR = "UNKNOWN_COMMAND";
+    public static final String PATH_COMMAND_AUTHORIZATION = "AUTHORIZATION_PAGE";
 
-	private RegistrationUser() {
-	}
+    private RegistrationUser() {
+    }
 
-	public static RegistrationUser getInstance() {
-		return instance;
-	}
+    public static RegistrationUser getInstance() {
+        return instance;
+    }
 
-	@Override
-	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RegistrationInfo information = new RegistrationInfo(request);
-		String path = PART_PATH + PATH_COMMAND_REGISTRATION;
-		request.getSession(true).setAttribute(SESSION_PATH, PATH_COMMAND_REGISTRATION);
-		try {
-			if (EMPTY_PASSWORD.equals(information.getEnteredPassword()) || !(information.getEnteredPassword().equals(information.getRepeatedPassword()))) {
-				response.sendRedirect(PART_PATH + PATH_COMMAND_REGISTRATION + "&incorrect_data_message=Incorrect data");
-				return;
-			}
-			
-			User user = USER_SERVISE.registration(information);
-			
-		if(user == null) {
-				response.sendRedirect(PART_PATH + PATH_COMMAND_REGISTRATION + "&email_is_busy=This user is already registered");
-				return;
-			}
-			request.getSession(true).setAttribute(SESSION_PATH, PATH_COMMAND_AUTHORIZATION);
-			path = PART_PATH + PATH_COMMAND_AUTHORIZATION + "&registration_message=You have been registered";
-		} catch (ServiseException e) {
-			e.printStackTrace();
-			request.getSession(true).setAttribute(SESSION_PATH, PATH_COMMAND_ERROR);
-			path = PART_PATH + PATH_COMMAND_ERROR;
-		}
-		response.sendRedirect(path);
-	}
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String path = PATH_COMMAND_ERROR;
+        RegistrationInfo information = new RegistrationInfo(request);
+        request.getSession(true).setAttribute(SESSION_PATH, PATH_COMMAND_REGISTRATION);
+        try {
+            if (EMPTY_PASSWORD.equals(information.getEnteredPassword()) || !(information.getEnteredPassword().equals(information.getRepeatedPassword()))) {
+                response.sendRedirect(PART_PATH + PATH_COMMAND_REGISTRATION + "&incorrect_data_message=Incorrect data");
+                return;
+            }
+
+            User user = USER_SERVISE.registration(information);
+
+            if (user == null) {
+                response.sendRedirect(PART_PATH + PATH_COMMAND_REGISTRATION + "&email_is_busy=This user is already registered");
+                return;
+            }
+            request.getSession(true).setAttribute(SESSION_PATH, PATH_COMMAND_AUTHORIZATION);
+            path = PATH_COMMAND_AUTHORIZATION + "&registration_message=You have been registered";
+        } catch (ServiseException e) {
+            e.printStackTrace();
+            request.getSession(true).setAttribute(SESSION_PATH, PATH_COMMAND_ERROR);
+        }
+        response.sendRedirect("Controller?command=" + path);
+    }
 }
