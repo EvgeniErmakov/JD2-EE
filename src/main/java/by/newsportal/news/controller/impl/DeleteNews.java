02b72bb5d9cbab74d5a75e9b9673756a6a4332b4
EvgeniErmakov <evgeni_ermakov@mail.ru>
2021-09-07 09:42:56 +0300

    refactoring Delete News file

--- src/main/java/by/newsportal/news/controller/impl/DeleteNews.java
+++ src/main/java/by/newsportal/news/controller/impl/DeleteNews.java
@@ -15,6 +15,7 @@ public class DeleteNews implements Command {
     private static final ServiceProvider PROVIDER = ServiceProvider.getInstance();
     private static final NewsService NEWS_SERVICE = PROVIDER.getNewsService();
     private static final DeleteNews INSTANCE = new DeleteNews();
+    public static final String ERROR_PAGE = "Controller?command=/WEB-INF/jsp/Error.jsp";
 
     public static DeleteNews getInstance() {
         return INSTANCE;
@@ -24,20 +25,18 @@ public class DeleteNews implements Command {
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession(false);
         String lastCommandName;
-        String path;
         int choosenNewsId = Integer.parseInt(request.getParameter("choosenNewsId"));
         int currentPage = Integer.parseInt(request.getParameter("currentPage"));
         if (choosenNewsId < 1) {
-            path = "/WEB-INF/jsp/Error.jsp";
-            session.setAttribute("lastURL", "UNKNOWN_COMMAND");
-            response.sendRedirect("Controller?command=" + path);
+            session.setAttribute("path", "UNKNOWN_COMMAND");
+            response.sendRedirect(ERROR_PAGE);
             return;
         }
 
         try {
             NEWS_SERVICE.deleteNews(choosenNewsId);
             lastCommandName = "AFTER_AUTHORIZATION&currentPage=" + currentPage;
-            session.setAttribute("lastURL", lastCommandName);
+            session.setAttribute("path", lastCommandName);
             response.sendRedirect("Controller?command=" + lastCommandName);
         } catch (ServiceException e) {
             e.printStackTrace();
